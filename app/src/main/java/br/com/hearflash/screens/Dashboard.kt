37d59f6c127a034306ec48214f230c0fb57cefac
Rafael Pereira Dias <p.rafael.dias@viveo.com.br>
2025-03-16 14:58:10 -0300

    feat: Update back arrow icon in Dashboard screen
    - Replaced the `ArrowBack` icon with the `AutoMirrored.Filled.ArrowBack` icon in the Dashboard screen for better right-to-left language support.

--- app/src/main/java/br/com/hearflash/screens/Dashboard.kt
+++ app/src/main/java/br/com/hearflash/screens/Dashboard.kt
@@ -19,7 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -55,7 +55,7 @@ fun Dashboard(
                 navigationIcon = {
                     IconButton(onClick = { navController.navigate("home") }) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Voltar para Home"
                         )
                     }
